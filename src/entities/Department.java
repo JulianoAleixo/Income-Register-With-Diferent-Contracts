117e054ea10158d3a38d45503143cd8750704433
JulianoAleixo <juliano.moreira.aleixo@gmail.com>
2024-01-27 16:46:04 -0300

    Add final program

---
+++ src/entities/Department.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class Department {
+	private String name;
+	
+	// Constructor
+	public Department() {
+	}
+
+	public Department(String name) {
+		this.name = name;
+	}
+	
+	// Getters and Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
